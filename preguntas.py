4e5cd4da7bf06fbebc6a8bae44ef04ee3101ace0
mapenarandam <mapenarandam@unal.edu.co>
2023-10-21 21:33:22 +0200

    gotenburg

--- preguntas.py
+++ preguntas.py
@@ -11,44 +11,37 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
-file_path = "data.csv"
 
 
-
-def read_data(path):
-    with open(file_path, "r") as file:
+def loading_data (name_file):
+    with open(name_file, "r") as file:
         data = file.readlines()
 
     data = [line.replace("\n", "") for line in data]
     data = [line.split("\t") for line in data]
-
     return data
+
 
 
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
-
     Rta/
     214
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    lista = []
-    
-    for i in range(0,len(data)):
-        numero = int(data[i][1])
-        lista.append(numero)
-    
-    return sum(lista)
+    val = 0
+    for line in data:
+        val += int(line[1])
+    val
+    return val
 
 
 def pregunta_02():
     """
     Retorne la cantidad de registros por cada letra de la primera columna como la lista
     de tuplas (letra, cantidad), ordendas alfabéticamente.
-
     Rta/
     [
         ("A", 8),
@@ -57,35 +50,23 @@ def pregunta_02():
         ("D", 6),
         ("E", 14),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    lista = []
-    
-    for i in range(0,len(data)):
-        letra = data[i][0]
-        lista.append(letra)
-    
-    cnt = Counter(lista)
-    
-    keys = list(cnt.keys())
-    keys.sort()
-    
-    lista_2 = []
-    
-    for j in keys:
-        query = (j, cnt[j])
-        lista_2.append(query) 
-    
-    return lista_2
+    letters = sorted(list(set([line[0] for line in data])))
+    dict_count = {letter: 0 for letter in letters}
+    for line in data:
+        letter = line[0]
+        dict_count[letter] = dict_count.get(letter, 0) + 1
+    list_count = [(k, v) for k, v in dict_count.items()]
+
+    return list_count
 
 
 def pregunta_03():
     """
     Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
     de tuplas (letra, suma) ordendas alfabeticamente.
-
     Rta/
     [
         ("A", 53),
@@ -94,51 +75,23 @@ def pregunta_03():
         ("D", 31),
         ("E", 67),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    list_a, list_b, list_c, list_d, list_e = [[],[],[],[],[]]
-    
-    lista = [list_a, list_b, list_c, list_d, list_e]
-    
-    text = "ABCDE"
-    
-    lista_2 = []
-    
-    for i in range(0,len(data)):
-        if data[i][0] == "A":
-            a = int(data[i][1])
-            list_a.append(a)
-            
-        elif data[i][0] == "B":
-            b = int(data[i][1])
-            list_b.append(b)
-            
-        elif data[i][0] == "C":
-            c = int(data[i][1])
-            list_c.append(c)
-            
-        elif data[i][0] == "D":
-            d = int(data[i][1])
-            list_d.append(d)
-            
-        else:
-            e = int(data[i][1])
-            list_e.append(e)
-    
-    for i in range(0,len(lista)):
-        tupla = (text[i], sum(lista[i]))
-        lista_2.append(tupla)
-    
-    return lista_2
+    letters = sorted(list(set([line[0] for line in data])))
+    dict_count = {letter: 0 for letter in letters}
+    for line in data:
+        letter = line[0]
+        val = int(line[1])
+        dict_count[letter] = dict_count.get(letter, 0) + val
+    list_count = [(k, v) for k, v in dict_count.items()]
+    return list_count
 
 
 def pregunta_04():
     """
     La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
     registros por cada mes, tal como se muestra a continuación.
-
     Rta/
     [
         ("01", 3),
@@ -154,15 +107,13 @@ def pregunta_04():
         ("11", 2),
         ("12", 3),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
     dict_count = {}
     for line in data:
         month = line[2].split("-")[1]
         dict_count[month] = dict_count.get(month, 0) + 1
     list_count = sorted([(k, v) for k, v in dict_count.items()])
-    
     return list_count
 
 
@@ -170,7 +121,6 @@ def pregunta_05():
     """
     Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
     letra de la columa 1.
-
     Rta/
     [
         ("A", 9, 2),
@@ -179,44 +129,22 @@ def pregunta_05():
         ("D", 8, 3),
         ("E", 9, 1),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    list_a, list_b, list_c, list_d, list_e = [[],[],[],[],[]]
-    
-    lista = [list_a, list_b, list_c, list_d, list_e]
-    
-    text = "ABCDE"
-    
-    lista_2 = []
-    
-    for i in range(0,len(data)):
-        if data[i][0] == "A":
-            a = int(data[i][1])
-            list_a.append(a)
-            
-        elif data[i][0] == "B":
-            b = int(data[i][1])
-            list_b.append(b)
-            
-        elif data[i][0] == "C":
-            c = int(data[i][1])
-            list_c.append(c)
-            
-        elif data[i][0] == "D":
-            d = int(data[i][1])
-            list_d.append(d)
-            
-        else:
-            e = int(data[i][1])
-            list_e.append(e)
-    
-    for i in range(0,len(lista)):
-        tupla = (text[i], max(lista[i]), min(lista[i]))
-        lista_2.append(tupla)
-    
-    return lista_2
+    letters = sorted(list(set([line[0] for line in data])))
+    dict_count = {letter: [0, 10] for letter in letters}
+    for line in data:
+        letter = line[0]
+        val = int(line[1])
+        max_, min_ = dict_count[letter]
+        if val > max_:
+            dict_count[letter][0] = val
+        if val < min_:
+            dict_count[letter][1] = val
+
+    list_count = [(k, *tuple(v)) for k, v in dict_count.items()]
+    return list_count
 
 
 def pregunta_06():
@@ -225,7 +153,6 @@ def pregunta_06():
     una clave y el valor despues del caracter `:` corresponde al valor asociado a la
     clave. Por cada clave, obtenga el valor asociado mas pequeño y el valor asociado mas
     grande computados sobre todo el archivo.
-
     Rta/
     [
         ("aaa", 1, 9),
@@ -239,81 +166,26 @@ def pregunta_06():
         ("iii", 0, 9),
         ("jjj", 5, 17),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    aaa,bbb,ccc,ddd,eee,fff,ggg,hhh,iii,jjj = [],[],[],[],[],[],[],[],[],[]
-      
-    response = []
-    
-    let = []
-    num = []
-        
-    for i in range(0, len(data)):
-        both = data[i][4].split(",")
-        
-        l = [j.split(":",1)[0] for j in both]
-        let.append(l)
-        
-        m = [k.split(":",1)[1] for k in both]
-        num.append(m)
-    
-    for i in range(0,len(num)):
-       for j in range(0,len(num[i])):
-           num[i][j] = int(num[i][j]) 
-    
-    for i in range(0,len(let)):
-        for j in range(0,len(let[i])):
-            if let[i][j] == "aaa":
-                a = int(num[i][j])
-                aaa.append(a)
-                
-            if let[i][j] == "bbb":
-                b = int(num[i][j])
-                bbb.append(b)
-                
-            if let[i][j] == "ccc":
-                c = int(num[i][j])
-                ccc.append(c)
-                
-            if let[i][j] == "ddd":
-                d = int(num[i][j])
-                ddd.append(d)
-                
-            if let[i][j] == "eee":
-                e = int(num[i][j])
-                eee.append(e)
-                
-            if let[i][j] == "fff":
-                f = int(num[i][j])
-                fff.append(f)
-                
-            if let[i][j] == "ggg":
-                g = int(num[i][j])
-                ggg.append(g)
-                
-            if let[i][j] == "hhh":
-                h = int(num[i][j])
-                hhh.append(h)
-                
-            if let[i][j] == "iii":
-                ii = int(num[i][j])
-                iii.append(ii)
-                
-            if let[i][j] == "jjj":
-                j = int(num[i][j])
-                jjj.append(j)
-                
-    lista = [aaa,bbb,ccc,ddd,eee,fff,ggg,hhh,iii,jjj]
-
-    names = ["aaa","bbb","ccc","ddd","eee","fff","ggg","hhh","iii","jjj"]
-                                     
-    for i in range(0,len(names)):
-        tup = (names[i], min(lista[i]), max(lista[i]))
-        response.append(tup)
-
-    return response
+    dict_count = {}
+    for line in data:
+        dict_ = {
+            string.split(":")[0]: string.split(":")[1] for string in line[4].split(",")
+        }
+        for k, v in dict_.items():
+            v = int(v)
+            if k in dict_count:
+                min_, max_ = dict_count[k]
+                if v > max_:
+                    dict_count[k][1] = v
+                if v < min_:
+                    dict_count[k][0] = v
+            else:
+                dict_count[k] = [v, v]
+    list_count = sorted([(k, *tuple(v)) for k, v in dict_count.items()])
+    return list_count
 
 
 def pregunta_07():
@@ -321,7 +193,6 @@ def pregunta_07():
     Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
     valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
     a dicho valor de la columna 2.
-
     Rta/
     [
         (0, ["C"]),
@@ -335,64 +206,21 @@ def pregunta_07():
         (8, ["E", "D", "E", "A", "B"]),
         (9, ["A", "B", "E", "A", "A", "C"]),
     ]
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    lista0, lista1, lista2, lista3, lista4, lista5, lista6, lista7, lista8, lista9 =[[],[],[],[],[],[],[],[],[],[]]
-    
-    lista = [lista0, lista1, lista2, lista3, lista4, lista5, lista6, lista7, lista8, lista9]
-    
-    lista_2 = []
-    
-    text = "0123456789"
-    
-    for i in range(0, len(data)):
-        if data[i][1] == "0":
-            zero = data[i][0]
-            lista0.append(zero)
-        
-        elif data[i][1] == "1":
-            one = data[i][0]
-            lista1.append(one)
-            
-        elif data[i][1] == "2":
-            two = data[i][0]
-            lista2.append(two)
-            
-        elif data[i][1] == "3":
-            three = data[i][0]
-            lista3.append(three)
-            
-        elif data[i][1] == "4":
-            four = data[i][0]
-            lista4.append(four)
-            
-        elif data[i][1] == "5":
-            five = data[i][0]
-            lista5.append(five)
-            
-        elif data[i][1] == "6":
-            six = data[i][0]
-            lista6.append(six)
-            
-        elif data[i][1] == "7":
-            seven = data[i][0]
-            lista7.append(seven)
-            
-        elif data[i][1] == "8":
-            eight = data[i][0]
-            lista8.append(eight)
-            
+    dict_count = {}
+    for line in data:
+        letter = line[0]
+        val = int(line[1])
+
+        if val in dict_count:
+            dict_count[val].append(letter)
         else:
-            nine = data[i][0]
-            lista9.append(nine)
-            
-    for i in range(0, len(text)):
-        tupla = (i, lista[i])
-        lista_2.append(tupla)
-    
-    return lista_2
+            dict_count[val] = [letter]
+    list_count = sorted([(k, v) for k, v in dict_count.items()])
+
+    return list_count
 
 
 def pregunta_08():
@@ -401,7 +229,6 @@ def pregunta_08():
     de la segunda columna; la segunda parte de la tupla es una lista con las letras
     (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
     valor de la segunda columna.
-
     Rta/
     [
         (0, ["C"]),
@@ -415,75 +242,17 @@ def pregunta_08():
         (8, ["A", "B", "D", "E"]),
         (9, ["A", "B", "C", "E"]),
     ]
-
     """
-    data = read_data(file_path)
 
-    lista0, lista1, lista2, lista3, lista4, lista5, lista6, lista7, lista8, lista9 =[[],[],[],[],[],[],[],[],[],[]]
-    
-    lista = [lista0, lista1, lista2, lista3, lista4, lista5, lista6, lista7, lista8, lista9]
-    
-    lista_2 = []
-    
-    text = "0123456789"
-    
-    for i in range(0, len(data)):
-        if data[i][1] == "0":
-            zero = data[i][0]
-            lista0.append(zero)
-        
-        elif data[i][1] == "1":
-            one = data[i][0]
-            lista1.append(one)
-            
-        elif data[i][1] == "2":
-            two = data[i][0]
-            lista2.append(two)
-            
-        elif data[i][1] == "3":
-            three = data[i][0]
-            lista3.append(three)
-            
-        elif data[i][1] == "4":
-            four = data[i][0]
-            lista4.append(four)
-            
-        elif data[i][1] == "5":
-            five = data[i][0]
-            lista5.append(five)
-            
-        elif data[i][1] == "6":
-            six = data[i][0]
-            lista6.append(six)
-            
-        elif data[i][1] == "7":
-            seven = data[i][0]
-            lista7.append(seven)
-            
-        elif data[i][1] == "8":
-            eight = data[i][0]
-            lista8.append(eight)
-            
-        else:
-            nine = data[i][0]
-            lista9.append(nine)
-    
-    for i in range(0,len(lista)):
-        lista[i] = sorted(list(set(lista[i])))
-        
-    
-    for i in range(0, len(text)):
-        tupla = (i, lista[i])
-        lista_2.append(tupla)
-    
-    return lista_2
+    list_count = pregunta_07()
+
+    return [(t[0], sorted(list(dict.fromkeys(t[1])))) for t in list_count]
 
 
 def pregunta_09():
     """
     Retorne un diccionario que contenga la cantidad de registros en que aparece cada
     clave de la columna 5.
-
     Rta/
     {
         "aaa": 13,
@@ -497,82 +266,24 @@ def pregunta_09():
         "iii": 18,
         "jjj": 18,
     }
-
     """
-   data = read_data(file_path)
-        
-    aaa, bbb, ccc, ddd, eee, fff, ggg, hhh, iii, jjj = [], [], [], [], [], [], [], [], [], []
-    
-    lista = [aaa, bbb, ccc, ddd, eee, fff, ggg, hhh, iii, jjj]
-    
-    total = []
-    
-    keys = ["aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj"]
-    
-    for i in range(0, len(data)):
-        if "aaa" in data[i][4]:
-            a = 1
-            aaa.append(a)
-    
-    for i in range(0, len(data)):
-        if "bbb" in data[i][4]:
-            b = 1
-            bbb.append(b)
-            
-    for i in range(0, len(data)):
-        if "ccc" in data[i][4]:
-            c = 1
-            ccc.append(c)
-            
-    for i in range(0, len(data)):
-        if "ddd" in data[i][4]:
-            d = 1
-            ddd.append(d)
-    
-    for i in range(0, len(data)):
-        if "eee" in data[i][4]:
-            e = 1
-            eee.append(e)
+    data = loading_data("data.csv")
 
-    for i in range(0, len(data)):
-        if "fff" in data[i][4]:
-            f = 1
-            fff.append(f)
-
-    for i in range(0, len(data)):
-        if "ggg" in data[i][4]:
-            g = 1
-            ggg.append(g)
-
-    for i in range(0, len(data)):
-        if "hhh" in data[i][4]:
-            h = 1
-            hhh.append(h)
-
-    for i in range(0, len(data)):
-        if "iii" in data[i][4]:
-            i = 1
-            iii.append(i)
-
-    for i in range(0, len(data)):
-        if "jjj" in data[i][4]:
-            j = 1
-            jjj.append(j)            
-          
-    for i in range(0, len(lista)):
-        t = len(lista[i])
-        total.append(t)
-    
-    dic = {keys[i]: total[i] for i in range(len(keys))}
-    
-    return dic
+    dict_count = {}
+    for line in data:
+        dict_ = {
+            string.split(":")[0]: string.split(":")[1] for string in line[4].split(",")
+        }
+        for k, v in dict_.items():
+            dict_count[k] = dict_count.get(k, 0) + 1
+    dict_count = {k: v for k, v in sorted(dict_count.items(), key=lambda item: item[0])}
+    return dict_count
 
 
 def pregunta_10():
     """
     Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
     cantidad de elementos de las columnas 4 y 5.
-
     Rta/
     [
         ("E", 3, 5),
@@ -583,36 +294,29 @@ def pregunta_10():
         ("E", 2, 3),
         ("E", 3, 3),
     ]
-
-
     """
-    data = read_data(file_path)
+    data = loading_data("data.csv")
 
-    elem4 = []
-    elem5 = []
-    
-    final = []
-    
-    for i in range(0, len(data)):
-        number = data[i][3].count(",") + 1
-        elem4.append(number)
-    
-    for i in range(0, len(data)):
-        number2 = data[i][4].count(",") + 1
-        elem5.append(number2)
-            
-    for i in range(0, len(data)):
-        f = (data[i][0], elem4[i], elem5[i])
-        final.append(f)
-    
-    return final
+    letters = sorted(list(set([line[0] for line in data])))
+
+    list_ = []
+    for line in data:
+        letter = line[0]
+        elements_4 = len(line[3].split(","))
+        elements_5 = len(
+            {
+                string.split(":")[0]: string.split(":")[1]
+                for string in line[4].split(",")
+            }
+        )
+        list_.append((letter, elements_4, elements_5))
+    return list_
 
 
 def pregunta_11():
     """
     Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
     columna 4, ordenadas alfabeticamente.
-
     Rta/
     {
         "a": 122,
@@ -623,56 +327,24 @@ def pregunta_11():
         "f": 134,
         "g": 35,
     }
-
-
     """
-    data = read_data(file_path)
-    
-    a,b,c,d,e,f,g = [],[],[],[],[],[],[]
-    
-    general = [a,b,c,d,e,f,g]
-    
-    name = "abcdefg"
-    
-    for i in range(0,len(data)):
-        if "a" in data[i][3]:
-            numa = int(data[i][1])
-            a.append(numa)
-        
-        if "b" in data[i][3]:
-            numb = int(data[i][1])
-            b.append(numb)
-            
-        if "c" in data[i][3]:
-            numc = int(data[i][1])
-            c.append(numc)
-            
-        if "d" in data[i][3]:
-            numd = int(data[i][1])
-            d.append(numd)
-            
-        if "e" in data[i][3]:
-            nume = int(data[i][1])
-            e.append(nume)
-            
-        if "f" in data[i][3]:
-            numf = int(data[i][1])
-            f.append(numf)
-            
-        if "g" in data[i][3]:
-            numg = int(data[i][1])
-            g.append(numg)
-            
-    dic = {name[i]: sum(general[i]) for i in range(len(name))}
-    
-    return dic
+    data = loading_data("data.csv")
+
+    dict_count = {}
+    for line in data:
+        letters = line[3].split(",")
+        val = int(line[1])
+        for letter in letters:
+            dict_count[letter] = dict_count.get(letter, 0) + val
+    dict_count = {k: v for k, v in sorted(dict_count.items(), key=lambda item: item[0])}
+
+    return dict_count
 
 
 def pregunta_12():
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
     los valores de la columna 5 sobre todo el archivo.
-
     Rta/
     {
         'A': 177,
@@ -681,12 +353,10 @@ def pregunta_12():
         'D': 136,
         'E': 324
     }
-
     """
-   data = read_data(file_path)
+    data = loading_data("data.csv")
 
     dict_count = {}
-
     for line in data:
         letter = line[0]
         dict_ = {
@@ -696,5 +366,4 @@ def pregunta_12():
 
         dict_count[letter] = dict_count.get(letter, 0) + sum_vals
     dict_count = {k: v for k, v in sorted(dict_count.items(), key=lambda item: item[0])}
-    
     return dict_count
